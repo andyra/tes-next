e41b17ffe547d41965612c73b575c2e18e7f9efc
Andy Smith <asmith.esq@gmail.com>
2022-03-11 20:40:26 -0600

    Set playing in multiple places

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
 import { useContext } from "react";
 import AppContext from "../components/AppContext";
 
+const DATA = [
+  {
+    title: "Gabriel's Friendship Gang",
+    url: "https://example.com/1",
+    album: 1
+  },
+  {
+    title: "Matt Slime",
+    url: "https://example.com/2",
+    album: 2
+  },
+  {
+    title: "Capestrano",
+    url: "https://example.com/3",
+    album: 3
+  }
+];
+
 export default function Home() {
   const value = useContext(AppContext);
+
+  function togglePlay() {
+    value.state.playing ? value.setPlaying(false) : value.setPlaying(true)
+  }
 
   return (
     <>
@@ -10,6 +32,21 @@ export default function Home() {
       <p>
         Playing is {`${value.state.playing}`}
       </p>
+      <ul className="border-t">
+        {DATA.map((item, i) =>
+          <li className="border-b py-8 flex justify-between">
+            <div className="flex items-center gap-8">
+              <button onClick={togglePlay} className="border rounded-full flex items-center justify-center h-32 px-12">
+                {value.state.playing ? "Pause" : "Play"}
+              </button>
+              {item.title}
+            </div>
+            <button className="border rounded-full flex items-center justify-center h-32 px-12">
+              Add
+            </button>
+          </li>
+        )}
+      </ul>
     </>
   )
 }
