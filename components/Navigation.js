2d5f0f0394e53d378c705d7273adf8eb47fd8670
Andy Smith <asmith.esq@gmail.com>
2022-03-30 18:31:24 -0500

    Fix icon problem

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,45 +2,56 @@ import Link from "next/link";
 import cn from "classnames";
 import Icon from "./Icon";
 
-const NavLink = ({
-  className,
-  count,
-  icon,
-  title,
-  url
-}) => {
+const NavLink = ({ className, count, icon, title, url }) => {
   const linkClasses = cn({
     "flex items-center gap-8 h-32 px-12 -mx-12 py-16 rounded-lg hover:bg-gray-50 dark:hover:bg-white/5": true,
-    [className]: className
+    [className]: className,
   });
 
   return (
     <Link href={url}>
       <a className={linkClasses}>
-        {icon &&
+        {icon && (
           <span className="w-16 flex items-center justify-center">
             <Icon name={icon} />
-          </span>}
+          </span>
+        )}
         <span className="flex-1">{title}</span>
       </a>
     </Link>
-  )
-}
+  );
+};
 
 const Navigation = () => (
   <nav className="row-span-1 flex flex-col gap-24 bg-white dark:bg-gray-800 rounded-lg p-16">
     <ul>
       <NavLink title="T.E.S." url="/" className="mb-24" />
-      <li><NavLink title="Episodes" url="/episodes" icon="mic-outline" /></li>
-      <li><NavLink title="Music" url="/music" icon="musical-notes-outline" /></li>
-      <li><NavLink title="Wiki" url="/wiki" icon="book-outline" /></li>
-      <li><NavLink title="About" url="/about" icon="information-outline" /></li>
+      <li>
+        <NavLink title="Episodes" url="/episodes" icon="mic" />
+      </li>
+      <li>
+        <NavLink title="Music" url="/music" icon="musical-notes" />
+      </li>
+      <li>
+        <NavLink title="Wiki" url="/wiki" icon="book" />
+      </li>
+      <li>
+        <NavLink title="About" url="/about" icon="information" />
+      </li>
     </ul>
     <ul>
-      <li><NavLink title="Add Playlist" url="/" icon="add-circle-outline"/></li>
-      <li><NavLink title="Favorites" url="/" icon="heart-outline" /></li>
-      <li><NavLink title="Boat Show 9" url="/" icon="musical-notes-outline" /></li>
-      <li><NavLink title="For this weekend" url="/" icon="musical-notes-outline" /></li>
+      <li>
+        <NavLink title="Add Playlist" url="/" icon="add-circle" />
+      </li>
+      <li>
+        <NavLink title="Favorites" url="/" icon="heart" />
+      </li>
+      <li>
+        <NavLink title="Boat Show 9" url="/" icon="musical-notes" />
+      </li>
+      <li>
+        <NavLink title="For this weekend" url="/" icon="musical-notes" />
+      </li>
     </ul>
   </nav>
 );
---
+++ constants.js
@@ -0,0 +1,123 @@
+export const ITEMS_TEST = [
+  {
+    track: {
+      id: 1,
+      title: "Gabriel's Friendship Gang",
+      url: "https://example.com/1",
+      album: 1,
+      artist: "Artist Name",
+    },
+    position: 1,
+    listType: "tracklist",
+  },
+  {
+    track: {
+      id: 2,
+      title: "Matt Slime",
+      url: "https://example.com/2",
+      album: 2,
+      artist: "Artist Name",
+    },
+    position: 2,
+    listType: "tracklist",
+  },
+  {
+    track: {
+      id: 3,
+      title: "Capestrano",
+      url: "https://example.com/3",
+      album: 3,
+      artist: "Artist Name",
+    },
+    position: 3,
+    listType: "tracklist",
+  },
+  {
+    track: {
+      id: 4,
+      title: "Napoleon",
+      url: "https://example.com/4",
+      album: 4,
+      artist: "Artist Name",
+    },
+    position: 4,
+    listType: "tracklist",
+  },
+  {
+    track: {
+      id: 5,
+      title: "Linda I Miss You",
+      url: "https://example.com/5",
+      album: 5,
+      artist: "Artist Name",
+    },
+    position: 5,
+    listType: "tracklist",
+  },
+  {
+    track: {
+      id: 1,
+      title: "Gabriel's Friendship Gang",
+      url: "https://example.com/1",
+      album: 1,
+      artist: "Artist Name",
+    },
+    position: 1,
+    listType: "tracklist",
+  },
+  {
+    track: {
+      id: 2,
+      title: "Matt Slime",
+      url: "https://example.com/2",
+      album: 2,
+      artist: "Artist Name",
+    },
+    position: 2,
+    listType: "tracklist",
+  },
+  {
+    track: {
+      id: 3,
+      title: "Capestrano",
+      url: "https://example.com/3",
+      album: 3,
+      artist: "Artist Name",
+    },
+    position: 3,
+    listType: "tracklist",
+  },
+  {
+    track: {
+      id: 1,
+      title: "Gabriel's Friendship Gang",
+      url: "https://example.com/1",
+      album: 1,
+      artist: "Artist Name",
+    },
+    position: 1,
+    listType: "tracklist",
+  },
+  {
+    track: {
+      id: 2,
+      title: "Matt Slime",
+      url: "https://example.com/2",
+      album: 2,
+      artist: "Artist Name",
+    },
+    position: 2,
+    listType: "tracklist",
+  },
+  {
+    track: {
+      id: 3,
+      title: "Capestrano",
+      url: "https://example.com/3",
+      album: 3,
+      artist: "Artist Name",
+    },
+    position: 3,
+    listType: "tracklist",
+  },
+];
