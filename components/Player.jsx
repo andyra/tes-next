fdb3e15612aee0f0ff47782350368bd3ea970b57
Andy Smith <asmith.esq@gmail.com>
2022-04-03 09:33:17 -0500

    Flatten audio player components

--- components/Player.jsx
+++ components/Player.jsx
@@ -4,24 +4,76 @@ import Button from "./Button";
 import Icon from "./Icon";
 import Queue from "./Queue";
 
-const OnDeck = ({onDeck}) => (
-  <div className={`flex items-center gap-8 w-1/4 transition ${onDeck ? "" : "opacity-0 pointer-events-none"}`}>
+// Components
+// ----------------------------------------------------------------------------
+
+const OnDeck = ({ onDeck }) => (
+  <div
+    className={`flex items-center gap-8 w-1/4 transition ${
+      onDeck ? "" : "opacity-0 pointer-events-none"
+    }`}
+  >
     <figure className="h-48 w-48 bg-blue-200 rounded flex items-center justify-center text-2xl">
       {onDeck ? onDeck.track.id : ""}
     </figure>
     <div>
-      <div className="text-sm font-medium">{onDeck ? onDeck.track.title : ""}</div>
-      <div className="text-xs text-gray-500">{onDeck ? onDeck.track.artist : ""}</div>
+      <div className="text-sm font-medium">
+        {onDeck ? onDeck.track.title : ""}
+      </div>
+      <div className="text-xs text-gray-500">
+        {onDeck ? onDeck.track.artist : ""}
+      </div>
     </div>
   </div>
 );
 
-export default function Player () {
+const PlayerControls = ({ back, isPlaying, next, togglePlay }) => (
+  <div className="flex-1 flex items-center justify-center gap-8">
+    <Button circle onClick={back}>
+      <Icon name="play-skip-back" solid />
+    </Button>
+    <Button
+      circle
+      size="lg"
+      onClick={togglePlay}
+      className={`${isPlaying ? "bg-green-200 hover:bg-green-300" : ""}`}
+    >
+      <Icon name={isPlaying ? "pause" : "play"} solid />
+    </Button>
+    <Button circle onClick={next}>
+      <Icon name="play-skip-forward" solid />
+    </Button>
+  </div>
+);
+
+const ExtraControls = ({ queueOpen, setQueueOpen }) => (
+  <div className="flex items-center justify-end gap-8 w-1/4">
+    <Button
+      circle
+      className={`${queueOpen ? "bg-green-200" : ""}`}
+      onClick={() => {
+        setQueueOpen(queueOpen ? false : true);
+      }}
+      aria-controls="queue"
+      aria-expanded={queueOpen ? false : true}
+      aria-label="Show Queue"
+    >
+      <Icon name="list-circle" />
+    </Button>
+  </div>
+);
+
+// Default
+// ----------------------------------------------------------------------------
+
+export default function Player() {
   const [queueOpen, setQueueOpen] = useState(false);
   const context = useContext(AppContext);
 
   function togglePlay() {
-    context.state.playing ? context.setPlaying(false) : context.setPlaying(true)
+    context.state.playing
+      ? context.setPlaying(false)
+      : context.setPlaying(true);
   }
 
   function isPlaying() {
@@ -74,43 +126,19 @@ export default function Player () {
     }
   }
 
-  const PlayerControls = () => (
-    <div className="flex-1 flex items-center justify-center gap-8">
-      <Button circle onClick={() => {back()}}>
-        <Icon name="play-skip-back" solid />
-      </Button>
-      <Button circle size="lg" onClick={togglePlay} className={`${isPlaying() ? "bg-green-200 hover:bg-green-300" : ""}`}>
-        <Icon name={isPlaying() ? "pause" : "play"} solid />
-      </Button>
-      <Button circle onClick={() => {next()}}>
-      <Icon name="play-skip-forward" solid />
-      </Button>
-    </div>
-  );
-
-  const ExtraControls = () => (
-    <div className="flex items-center justify-end gap-8 w-1/4">
-      <Button
-        circle
-        className={`${queueOpen ? "bg-green-200" : ""}`}
-        onClick={() => {setQueueOpen(queueOpen ? false : true)}}
-        aria-controls="queue"
-        aria-expanded={queueOpen ? false : true}
-        aria-label="Show Queue"
-      >
-        <Icon name="list-circle" />
-      </Button>
-    </div>
-  );
-
   return (
     <>
       <aside className="col-span-2 bg-white dark:bg-gray-800 p-8 rounded-lg flex items-center justify-between">
         <OnDeck onDeck={context.state.onDeck} />
-        <PlayerControls />
-        <ExtraControls />
+        <PlayerControls
+          back={back}
+          isPlaying={isPlaying()}
+          next={next}
+          togglePlay={togglePlay}
+        />
+        <ExtraControls queueOpen={queueOpen} setQueueOpen={setQueueOpen} />
       </aside>
       <Queue open={queueOpen} />
     </>
-  )
+  );
 }
