974ef8e6da3b1a45815f726a60d41a75ffb50120
Andy Smith <asmith.esq@gmail.com>
2022-04-29 15:48:01 -0500

    WIP

--- components/Player.jsx
+++ components/Player.jsx
@@ -11,17 +11,17 @@ import Queue from "./Queue";
 // Components
 // ----------------------------------------------------------------------------
 
-const OnDeck = ({ isFullScreen, isMobile, onDeck, setIsFullScreen }) => {
+const OnDeck = ({ isFullscreen, isMobile, onDeck, setIsFullscreen }) => {
   const onDeckClasses = cn({
     "flex items-center relative bg-red-200": true,
-    "gap-8 w-full md:w-1/3": !isFullScreen,
-    "gap-48 mb-48": isFullScreen
+    "gap-8 w-full md:w-1/3": !isFullscreen,
+    "w-full gap-48 mb-48": isFullscreen
   });
 
   const coverArtClasses = cn({
     "bg-primary-10 rounded flex items-center justify-center text-2xl flex-shrink-0 overflow-hidden": true,
-    "h-40 w-40 md:h-64 md:w-64": !isFullScreen,
-    "h-256 w-256": isFullScreen
+    "h-40 w-40 md:h-64 md:w-64": !isFullscreen,
+    "h-256 w-256": isFullscreen
   });
 
   const actionClasses = cn({
@@ -31,14 +31,14 @@ const OnDeck = ({ isFullScreen, isMobile, onDeck, setIsFullScreen }) => {
 
   const titleClasses = cn({
     "opacity-0": !onDeck,
-    "text-sm font-medium transition duration-300": !isFullScreen,
-    "text-7xl font-bold mb-16": isFullScreen
+    "text-sm font-medium transition duration-300": !isFullscreen,
+    "text-7xl font-bold mb-16": isFullscreen
   });
 
   const artistClasses = cn({
     "opacity-0": !onDeck,
-    "text-xs text-primary-50 transition duration-300": !isFullScreen,
-    "text-3xl font-medium": isFullScreen
+    "text-xs text-primary-50 transition duration-300": !isFullscreen,
+    "text-3xl font-medium": isFullscreen
   });
 
   return (
@@ -48,8 +48,8 @@ const OnDeck = ({ isFullScreen, isMobile, onDeck, setIsFullScreen }) => {
           <Image
             alt={`${onDeck.collection.title} cover art`}
             src={onDeck.collection.coverArtUrl}
-            width={isFullScreen ? 256 : isMobile ? 40 : 64}
-            height={isFullScreen ? 256 : isMobile ? 40 : 64}
+            width={isFullscreen ? 256 : isMobile ? 40 : 64}
+            height={isFullscreen ? 256 : isMobile ? 40 : 64}
           />
         )}
       </figure>
@@ -57,7 +57,7 @@ const OnDeck = ({ isFullScreen, isMobile, onDeck, setIsFullScreen }) => {
         <div className={titleClasses}>{onDeck ? onDeck.title : ""}</div>
         <div className={artistClasses}>{onDeck ? onDeck.artist.title : ""}</div>
       </div>
-      {(isFullScreen || !isMobile) && (
+      {(isFullscreen || !isMobile) && (
         <div id="actions" className={actionClasses}>
           <Button circle ghost>
             <Icon name="heart" />
@@ -67,11 +67,11 @@ const OnDeck = ({ isFullScreen, isMobile, onDeck, setIsFullScreen }) => {
           </Button>
         </div>
       )}
-      {isMobile && !isFullScreen && (
+      {isMobile && !isFullscreen && (
         <button
           className="absolute -top-8 right-0 -bottom-16 -left-8"
           onClick={() => {
-            setIsFullScreen(true);
+            setIsFullscreen(true);
           }}
         />
       )}
@@ -81,8 +81,7 @@ const OnDeck = ({ isFullScreen, isMobile, onDeck, setIsFullScreen }) => {
 
 const PlayerControls = ({
   back,
-  isFullScreen,
-  isMobile,
+  isFullscreen,
   isPlaying,
   next,
   onDeck,
@@ -90,8 +89,8 @@ const PlayerControls = ({
 }) => {
   const PlayerControlClasses = cn({
     "flex flex-col gap-4 bg-cyan-200": true,
-    "md:w-1/3": !isFullScreen,
-    "py-96 transition duration-300": isFullScreen
+    "md:w-1/3": !isFullscreen,
+    "w-full transition duration-300": isFullscreen
   });
 
   return (
@@ -123,92 +122,143 @@ const PlayerControls = ({
           <Icon name="play-skip-forward" solid />
         </Button>
       </div>
-      <PlaybackBar isFullScreen={isFullScreen} onDeck={onDeck} />
+      {!isFullscreen && (
+        <PlaybackBar isFullscreen={isFullscreen} onDeck={onDeck} />
+      )}
     </div>
   );
 };
 
-const PlaybackBar = ({ isFullScreen, onDeck }) => {
+const PlaybackBar = ({ isFullscreen, onDeck }) => {
   const containerClasses = cn({
-    "flex items-center gap-8 col-span-2 transition bg-orange-200": true,
-    "absolute left-0 bottom-0 right-0 md:static": true,
-    "w-auto": true,
-    "opacity-0 md:opacity-100": !onDeck
+    "grid gap-8 items-center col-span-2 transition bg-orange-200": true,
+    "opacity-0 md:opacity-100": !onDeck,
+    "grid-cols-[1fr] grid-rows-[4px] md:grid-cols-[40px,1fr,40px] md:grid-rows-[8px,1fr] absolute left-0 bottom-0 right-0 w-auto md:static": !isFullscreen,
+    "w-full": isFullscreen
   });
 
   const timeClasses = cn({
     "min-w-40 text-xs text-primary-50 bg-purple-200": true,
-    "hidden md:block": !isFullScreen
+    "hidden md:block row-span-2": !isFullscreen
+  });
+
+  const elapsedClasses = cn({
+    [timeClasses]: true,
+    "text-right": !isFullscreen,
+    "col-start-1 row-start-2": isFullscreen
+  });
+
+  const durationClasses = cn({
+    [timeClasses]: true,
+    "col-start-3 row-start-2 text-right": isFullscreen
+  });
+
+  const barClasses = cn({
+    "flex-1 h-4 bg-primary-25 rounded-full relative": true,
+    "md:col-span-1 md:row-span-2": !isFullscreen,
+    "col-start-1 col-span-3": isFullscreen
   });
 
   return (
     <div className={containerClasses}>
-      <time className={`${timeClasses} text-right`}>0:00</time>
-      <div className="flex-1 h-4 bg-primary-25 rounded-full" />
-      <time className={timeClasses}>0:00</time>
+      <time className={`${elapsedClasses}`}>0:00</time>
+      <div className={barClasses}>
+        <span className="bg-accent absolute left-0 top-0 bottom-0 right-1/2 rounded-full" />
+      </div>
+      <time className={durationClasses}>0:00</time>
     </div>
   );
 };
 
 const ExtraControls = ({
-  isFullScreen,
-  isMobile,
+  isFullscreen,
   onDeck,
   queueCount,
   queueIsOpen,
-  setIsFullScreen,
+  setIsFullscreen,
   setQueueIsOpen
 }) => {
   const containerClasses = cn({
     "flex-1 flex items-center justify-end gap-8 bg-yellow-200": true,
-    "absolute top-96 right-96": isFullScreen,
-    "hidden md:flex md:w-1/3": !isFullScreen
-  });
-
-  const queueButtonClasses = cn({
-    "bg-accent": queueIsOpen,
-    hidden: isFullScreen
+    "absolute top-96 right-96": isFullscreen,
+    "hidden md:flex md:w-1/3": !isFullscreen
   });
 
   return (
     <div className={containerClasses}>
-      <Button
-        circle
-        disabled={!onDeck && queueCount === 0}
-        onClick={() => {
-          setQueueIsOpen(false);
-          setIsFullScreen(!isFullScreen);
-        }}
-        aria-controls="full-screen"
-        aria-expanded={!isFullScreen}
-        aria-label="Full Screen"
-      >
-        <Icon name={isFullScreen ? "close" : "chevron-up"} solid />
-      </Button>
-      <Button
-        circle
-        className={queueButtonClasses}
-        disabled={!onDeck && queueCount === 0}
-        onClick={() => {
-          setIsFullScreen(false);
-          setQueueIsOpen(!queueIsOpen);
-        }}
-        aria-controls="queue"
-        aria-expanded={!queueIsOpen}
-        aria-label="Show Queue"
-      >
-        <Icon name={queueIsOpen ? "close" : "list"} solid />
-      </Button>
+      <FullscreenButton
+        isFullscreen={isFullscreen}
+        onDeck={onDeck}
+        queueCount={queueCount}
+        queueIsOpen={queueIsOpen}
+        setIsFullscreen={setIsFullscreen}
+        setQueueIsOpen={setQueueIsOpen}
+      />
+      <QueueButton
+        isFullscreen={isFullscreen}
+        onDeck={onDeck}
+        queueCount={queueCount}
+        queueIsOpen={queueIsOpen}
+        setIsFullscreen={setIsFullscreen}
+        setQueueIsOpen={setQueueIsOpen}
+      />
     </div>
   );
 };
+
+const FullscreenButton = ({
+  isFullscreen,
+  onDeck,
+  queueCount,
+  queueIsOpen,
+  setIsFullscreen,
+  setQueueIsOpen
+}) => (
+  <Button
+    circle
+    disabled={!onDeck && queueCount === 0}
+    onClick={() => {
+      setQueueIsOpen(false);
+      setIsFullscreen(!isFullscreen);
+    }}
+    aria-controls="full-screen"
+    aria-expanded={!isFullscreen}
+    aria-label="Full Screen"
+  >
+    <Icon name={isFullscreen ? "close" : "chevron-up"} solid />
+  </Button>
+);
+
+const QueueButton = ({
+  isFullscreen,
+  onDeck,
+  queueCount,
+  queueIsOpen,
+  setIsFullscreen,
+  setQueueIsOpen
+}) => (
+  <Button
+    circle
+    className={queueIsOpen ? "bg-accent" : ""}
+    disabled={!onDeck && queueCount === 0}
+    onClick={() => {
+      setIsFullscreen(false);
+      setQueueIsOpen(!queueIsOpen);
+    }}
+    aria-controls="queue"
+    aria-expanded={!queueIsOpen}
+    aria-label="Show Queue"
+  >
+    <Icon name={queueIsOpen ? "close" : "list"} solid />
+  </Button>
+);
 
 // Default
 // ----------------------------------------------------------------------------
 
 export default function Player() {
   const [queueIsOpen, setQueueIsOpen] = useState(false);
-  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isFullscreen, setIsFullscreen] = useState(false);
   const context = useContext(AudioContext);
   const { nextFrom, onDeck, playing, prevFrom, queue } = context.state;
   const { setNextFrom, setOnDeck, setPlaying, setPrevFrom, setQueue } = context;
@@ -219,7 +269,7 @@ export default function Player() {
   useEffect(() => {
     if (!onDeck && queueCount === 0) {
       setQueueIsOpen(false);
-      setIsFullScreen(false);
+      // setIsFullscreen(false);
     }
   }, [onDeck, queueCount]);
 
@@ -274,14 +324,16 @@ export default function Player() {
   }
 
   const playerClasses = cn({
-    "flex items-center gap-8 px-8 relative": true,
+    "flex items-center gap-8 px-8": true,
     "rounded-lg border shadow mx-8": true,
-    "md:col-span-2 md:border-none md:border-t md:shadow-none md:mx-0": true
+    "md:col-span-2 md:border-none md:border-t md:shadow-none md:mx-0": true,
+
+    relative: !isFullscreen,
+    "absolute z-50 top-0 left-0 w-full h-full flex-col justify-end bg-ground px-96": isFullscreen
 
     // "grid grid-cols-[1fr,48px] grid-rows-[1fr,8px]": true
     // "md:flex md:items-center md:justify-between": true,
-    // "col-span-2 p-8 gap-4 md:gap-8 border mx-4 mb-4 rounded-lg border-primary-10 md:border-t md:m-0 md:rounded-none": !isFullScreen,
-    // "flex flex-col justify-end absolute z-50 top-0 left-0 w-full h-full bg-ground pt-96 px-96": isFullScreen
+    // "col-span-2 p-8 gap-4 md:gap-8 border mx-4 mb-4 rounded-lg border-primary-10 md:border-t md:m-0 md:rounded-none": !isFullscreen,
   });
 
   return (
@@ -289,28 +341,31 @@ export default function Player() {
       <aside className={playerClasses}>
         <OnDeck
           onDeck={onDeck}
-          isFullScreen={isFullScreen}
+          isFullscreen={isFullscreen}
           isMobile={isMobile}
-          setIsFullScreen={setIsFullScreen}
+          setIsFullscreen={setIsFullscreen}
         />
+        {isFullscreen && (
+          <PlaybackBar isFullscreen={isFullscreen} onDeck={onDeck} />
+        )}
         <PlayerControls
           back={back}
-          isFullScreen={isFullScreen}
-          isMobile={isMobile}
+          isFullscreen={isFullscreen}
           isPlaying={isPlaying()}
           next={next}
           onDeck={onDeck}
           togglePlay={togglePlay}
         />
-        <ExtraControls
-          isFullScreen={isFullScreen}
-          isMobile={isMobile}
-          onDeck={onDeck}
-          queueCount={queueCount}
-          queueIsOpen={queueIsOpen}
-          setIsFullScreen={setIsFullScreen}
-          setQueueIsOpen={setQueueIsOpen}
-        />
+        {!isFullscreen && (
+          <ExtraControls
+            isFullscreen={isFullscreen}
+            onDeck={onDeck}
+            queueCount={queueCount}
+            queueIsOpen={queueIsOpen}
+            setIsFullscreen={setIsFullscreen}
+            setQueueIsOpen={setQueueIsOpen}
+          />
+        )}
       </aside>
       <Queue queueIsOpen={queueIsOpen} setQueueIsOpen={setQueueIsOpen} />
     </>
