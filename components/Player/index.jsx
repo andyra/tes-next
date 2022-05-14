4749ff3635d788d0b980b75ceab594f3746e2661
Andy Smith <asmith.esq@gmail.com>
2022-05-13 20:48:53 -0400

    format duration

--- components/Player/index.jsx
+++ components/Player/index.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { Transition } from "@headlessui/react";
 import { useMediaQuery } from "react-responsive";
+import Moment from "moment";
 import cn from "classnames";
 import { usePlayerContext } from "../../context/PlayerContext";
 import CurrentTrack from "./CurrentTrack";
@@ -9,6 +10,22 @@ import ExtraControls from "./ExtraControls";
 import MediaQuery, { BREAKPOINTS } from "../MediaQuery";
 import PlayerControls from "./PlayerControls";
 import Queue from "../Queue";
+
+// TODO: articles
+// https://codesandbox.io/s/5wwj02qy7k?file=/src/index.js
+// https://codesandbox.io/s/xj4897445q?file=/src/components/Player/Audio.js
+// https://dev.to/nicomartin/how-to-create-a-progressive-audio-player-with-react-hooks-31l1
+// http://goldfirestudios.com/proj/howlerjs/sound.ogg
+
+// autoplay: false
+// controls: false
+// loop: false
+// preload: metadata
+// src: you know
+// timeupdate: set state currentTime
+// seeking: set state isPlaying to false
+// seeked: set state isPlaying to true
+// ratechange: set state rate
 
 // Default
 // -----------------------------------------------------------------------------
@@ -33,6 +50,19 @@ export default function Player() {
   const [isLooped, setIsLooped] = useState(false);
   const [isRandom, setIsRandom] = useState(false);
 
+  // Refs
+  // https://letsbuildui.dev/articles/building-an-audio-player-with-react-hooks
+  // TODO • duration
+  // TODO • elapsed
+  // TODO • rate
+  // TODO • loading?
+  const audioSrc = "http://goldfirestudios.com/proj/howlerjs/sound.ogg";
+  const audioRef = useRef(typeof Audio !== "undefined" && new Audio(audioSrc));
+  const intervalRef = useRef();
+  const isReady = useRef(false);
+  const { duration } = audioRef.current;
+  const [trackProgress, setTrackProgress] = useState(0);
+
   // Config
   const playerIsEmpty =
     !currentTrack && prevList.length + nextList.length + queueList.length === 0;
@@ -54,6 +84,41 @@ export default function Player() {
       setIsFullscreen(false);
     }
   }, [playerIsEmpty]);
+
+  // Toggle playing
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+      startTimer();
+    } else {
+      clearInterval(intervalRef.current);
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  // Pause and clean up on unmount
+  useEffect(() => {
+    return () => {
+      audioRef.current.pause();
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  // Handle setup when changing tracks
+  useEffect(() => {
+    audioRef.current.pause();
+    audioRef.current = new Audio(audioSrc);
+    setTrackProgress(audioRef.current.currentTime);
+
+    if (isReady.current) {
+      audioRef.current.play();
+      setIsPlaying(true);
+      startTimer();
+    } else {
+      // Set the isReady ref as true for the next pass
+      isReady.current = true;
+    }
+  }, [currentTrack]);
 
   // Functions
   // ---------------------------------------------------------------------------
@@ -110,6 +175,50 @@ export default function Player() {
     setIsRandom(!isRandom);
   }
 
+  function startTimer() {
+    // Clear any timers already running
+    clearInterval(intervalRef.current);
+
+    intervalRef.current = setInterval(() => {
+      if (audioRef.current.ended) {
+        skipNext();
+      } else {
+        setTrackProgress(audioRef.current.currentTime);
+      }
+    }, [1000]);
+  }
+
+  function onScrub(value) {
+    // Clear any timers already running
+    clearInterval(intervalRef.current);
+    audioRef.current.currentTime = value;
+    setTrackProgress(audioRef.current.currentTime);
+  }
+
+  function onScrubEnd() {
+    // If not already playing, start
+    if (!isPlaying) {
+      setIsPlaying(true);
+    }
+    startTimer();
+  }
+
+  const currentPercentage = duration
+    ? `${(trackProgress / duration) * 100}%`
+    : "0%";
+  const trackStyling = `
+    -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #fff), color-stop(${currentPercentage}, #777))
+  `;
+
+  function formatDuration(duration) {
+    const d = Moment.duration(duration, "seconds");
+    const { hours, minutes, seconds } = d._data;
+    const h = hours > 0 ? `${hours}:` : "";
+    const m = `${minutes > 0 ? minutes : "0"}:`;
+    const s = seconds > 9 ? seconds : `0${seconds}`;
+    return `${h}${m}${s}`;
+  }
+
   // Render
   // ---------------------------------------------------------------------------
 
@@ -122,6 +231,20 @@ export default function Player() {
           currentTrack={currentTrack}
           setIsFullscreen={setIsFullscreen}
         />
+        <input
+          className="h-8 w-320 bg-accent cursor-pointer appearance-none"
+          type="range"
+          value={trackProgress}
+          step="1"
+          min="0"
+          max={duration ? duration : `${duration}`}
+          className="progress"
+          onChange={e => onScrub(e.target.value)}
+          onMouseUp={onScrubEnd}
+          onKeyUp={onScrubEnd}
+          style={{ background: trackStyling }}
+        />
+        <mark>{formatDuration(duration)}</mark>
         <PlayerControls
           isFullscreen={isFullscreen}
           isLooped={isLooped}
