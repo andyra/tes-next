847f8ebe435b948fefb53e3c9357ed5cf8ed19c8
Andy Smith <asmith.esq@gmail.com>
2022-06-05 16:17:51 -0500

    A great setlist computor

--- components/Badge.jsx
+++ components/Badge.jsx
@@ -1,7 +1,10 @@
 import cn from "classnames";
 
-const Badge = ({ children, className }) => {
-  const classes = cn("px-12 relative", className);
+const Badge = ({ children, className, relative = true }) => {
+  const classes = cn(
+    "flex items-center justify-center gap-4 px-8 rounded-full w-fit relative",
+    className
+  );
   return (
     <span className={classes}>
       <span className="absolute top-0 left-0 w-full h-full rounded-full border border-current opacity-25 pointer-events-none" />
--- pages/setlist.jsx
+++ pages/setlist.jsx
@@ -1,13 +1,32 @@
 import { useState } from "react";
+import Link from "next/link";
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
-import Link from "next/link";
+import * as Accordion from "@radix-ui/react-accordion";
+import cn from "classnames";
 import Badge from "../components/Badge";
 import Button from "../components/Button";
 import Icon from "../components/Icon";
 import Input from "../components/Input";
 import PageHeader, { PageTitle } from "../components/PageHeader";
 import { getCollectionType, normalizeTrack, shuffle } from "../helpers/";
+
+// Functions
+// ----------------------------------------------------------------------------
+
+// Creates an array of tracks to include bleeds/strategies on (i.e. [0, 9, 2])
+function getExtras(songCount, count, skipLastItem = true) {
+  let songIndexes = [...Array(songCount).keys()];
+  if (skipLastItem) {
+    songIndexes.pop();
+  }
+  if (songIndexes && count) {
+    const shuffledIndexes = shuffle(songIndexes);
+    return shuffledIndexes.splice(0, count);
+  }
+
+  return [];
+}
 
 // Components
 // ----------------------------------------------------------------------------
@@ -25,23 +44,11 @@ const Computor = ({
   songs,
   strategyCount
 }) => {
-  // Creates an array of tracks to include bleeds/strategies on (i.e. [0, 9, 2])
-  function getTrackExtras(songCount, count, lastItem) {
-    const maxTracks = lastItem ? songCount : songCount - 1;
-
-    if (songCount && count) {
-      const shuffledTrackIndexes = shuffle([...Array(maxTracks).keys()]);
-      return shuffledTrackIndexes.splice(0, count);
-    }
-
-    return [];
-  }
-
   function handleCompute() {
-    const shuffledSongs = shuffle(songs);
+    const shuffledSongs = shuffle([...songs]);
     const mySongs = shuffledSongs.splice(0, songCount);
-    const bleeds = getTrackExtras(songCount, bleedCount);
-    const strategies = getTrackExtras(songCount, strategyCount, true);
+    const bleeds = getExtras(songCount, bleedCount);
+    const strategies = getExtras(songCount, strategyCount, false);
     const items = mySongs.map((song, i) => ({
       song: song,
       bleed: bleeds.indexOf(i) >= 0,
@@ -74,7 +81,7 @@ const Computor = ({
           inputClassName="text-center"
           label="Bleeds"
           labelClassName={LABEL_CLASSES}
-          max={songCount}
+          max={songCount - 1}
           min={0}
           name="bleeds"
           onChange={e => {
@@ -109,48 +116,81 @@ const Computor = ({
   );
 };
 
+const SetlistItem = ({ item, i }) => {
+  const { bleed, song, strategy } = item;
+  const classes = cn({
+    "w-full py-12 px-16 rounded-lg hover:bg-primary-5 text-left transition group": true
+  });
+  return (
+    <Accordion.Item className="relative" value={song.title}>
+      {bleed && (
+        <div className="w-2 border-l-2 border-primary-50 border-dashed absolute top-32 -bottom-32 left-32 rounded-full flex items-center justify-center transform -translate-x-1/2" />
+      )}
+      <Accordion.Trigger className={classes}>
+        <div className="flex items-center gap-16">
+          <span className="flex items-center justify-center w-32 h-32 text-lg font-medium text-primary-50 text-center border-2 bg-ground border-primary-50 rounded-full relative">
+            {i + 1}
+          </span>
+          <div className="flex-1 text-5xl">{song.title}</div>
+          <div className="flex items-center gap-4">
+            {song.lyrics && (
+              <Badge className="text-sm text-primary-50 opacity-0 group-hover:opacity-100 transition">
+                Lyrics
+              </Badge>
+            )}
+            {song.notation && (
+              <Badge className="text-sm text-primary-50 opacity-0 group-hover:opacity-100 transition">
+                Notation
+              </Badge>
+            )}
+          </div>
+        </div>
+        {strategy && <div className="ml-40 opacity-50">"Strategy here"</div>}
+      </Accordion.Trigger>
+      <Accordion.Content className="ml-48">
+        <div className="lg:grid lg:grid-cols-2 gap-16 rounded-lg p-16 border border-primary-10">
+          {song.lyrics && (
+            <div>
+              <h3
+                className={`font-semibold text-xl mb-8${
+                  song.lyrics ? "" : " text-primary-50"
+                }`}
+              >
+                Lyrics
+              </h3>
+              <div
+                className="font-mono text-sm"
+                dangerouslySetInnerHTML={{ __html: song.lyrics }}
+              />
+            </div>
+          )}
+          {song.notation && (
+            <div>
+              <h3
+                className={`font-semibold text-xl mb-8${
+                  song.notation ? "" : " text-primary-50"
+                }`}
+              >
+                Notation
+              </h3>
+              <div
+                className="font-mono text-sm"
+                dangerouslySetInnerHTML={{ __html: song.notation }}
+              />
+            </div>
+          )}
+        </div>
+      </Accordion.Content>
+    </Accordion.Item>
+  );
+};
 const SetlistItems = ({ items }) =>
   items.length ? (
-    <ol className="list-decimal">
+    <Accordion.Root collapsible={true}>
       {items.map((item, i) => (
-        <li className="relative" key={i}>
-          {console.log(item)}
-          <Link href={item.song.uri}>
-            <a
-              className="flex items-center gap-8 hover:bg-primary-5 py-8 px-16 rounded-lg group"
-              noopener
-              noreferrer
-              target="_blank"
-            >
-              <div className="flex-1">
-                <div className="text-3xl font-medium">{item.song.title}</div>
-                {item.strategy && (
-                  <div className="opacity-50">"{item.strategy}"</div>
-                )}
-                {item.bleed && (
-                  <span className="h-24 px-8 border border-accent-25 text-accent text-sm rounded-full inline-flex gap-4 items-center justify-center transform translate-y-1/3">
-                    <Icon name="ArrowDown" />
-                    Bleed
-                  </span>
-                )}
-              </div>
-              <div className="flex items-center gap-4">
-                {item.song.lyrics && (
-                  <Badge className="text-sm text-primary-50 opacity-0 group-hover:opacity-100 transition">
-                    Lyrics
-                  </Badge>
-                )}
-                {item.song.notation && (
-                  <Badge className="text-sm text-primary-50 opacity-0 group-hover:opacity-100 transition">
-                    Notation
-                  </Badge>
-                )}
-              </div>
-            </a>
-          </Link>
-        </li>
+        <SetlistItem item={item} i={i} key={i} />
       ))}
-    </ol>
+    </Accordion.Root>
   ) : null;
 
 // Default
