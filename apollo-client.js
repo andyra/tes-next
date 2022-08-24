2186f255870ba43d52eaa88c8e8e0915ea2c643d
Andy Smith <asmith.esq@gmail.com>
2022-08-24 18:16:02 -0500

    Try hard-coding API endpoint

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.CRAFT_CMS_ENDPOINT,
+  uri: "https://content.tes.fm/api",
   cache: new InMemoryCache()
 });
 
