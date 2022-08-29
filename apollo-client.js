840e91b0cc879625478248556131597064d7a480
Andy Smith <asmith.esq@gmail.com>
2022-08-29 14:52:19 -0500

    Fix API endpoint

--- apollo-client.js
+++ apollo-client.js
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  // uri: "https://content.tes.fm/api",
-  uri: "https://tes-craft.test/api",
+  uri: "https://content.tes.fm/api",
+  // uri: "https://tes-craft.test/api",
   cache: new InMemoryCache()
 });
 
