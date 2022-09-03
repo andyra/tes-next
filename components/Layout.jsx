687d706f46124b62599e582780be0ad3afe4263c
Andy Smith <asmith.esq@gmail.com>
2022-09-03 11:18:10 -0500

    Add favicons

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -94,9 +94,23 @@ export default function Layout({ children, ...props }) {
           <meta property="og:description" content={metaDescription} />
         )}
         {/* Favicons and PWA Manifest */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#49249a" />
       </Head>
