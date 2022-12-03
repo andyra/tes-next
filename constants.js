1e8a422c0251282363c071c269ea6cd13a59a98e
Andy Smith <asmith.esq@gmail.com>
2022-12-03 14:23:50 -0600

    Add podcast links

--- constants.js
+++ constants.js
@@ -1,16 +1,30 @@
 import { gql } from "@apollo/client";
 
-// Contact
-// ----------------------------------------------------------------------------
-
 export const PHONE = "(325) 701-9997";
 export const EMAIL = "tes.thiseveningsshow@gmail.com";
 export const ADDRESS = {
   street: "905 N Mockingbird Lane",
   city: "Abilene",
   state: "TX",
-  zip: "79601"
+  zip: "79601",
 };
+export const PODCAST_LINKS = [
+  {
+    href: "https://tes.fm/feed.xml",
+    icon: "Rss",
+    title: "RSS",
+  },
+  {
+    href: "https://itunes.apple.com/us/podcast/this-evenings-show/id958245203?mt=2",
+    icon: "ApplePodcasts",
+    title: "Apple Podcasts",
+  },
+  {
+    href: "https://open.spotify.com/show/3wt4UxiB5KC4KFmAVFYwPA?si=l2GV6QFkTyyEK25_p7kutw",
+    icon: "Spotify",
+    title: "Spotify",
+  },
+];
 
 // Queries
 // ----------------------------------------------------------------------------
